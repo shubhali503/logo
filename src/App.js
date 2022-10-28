a63d67955dd3c1d6754b341ba4e64b00cfc4aecc
shubhali503 <shubhaliagarwal555@gmail.com>
2022-10-28 21:00:02 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='logo'>
+      <div className="logo-content">
+        <h1>HTML<span>&</span>CSS</h1>
+        <p>design and build websites</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
